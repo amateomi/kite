04fe5e72d88851d4beae622ccf41645da40a4df6
Andrey Sikorin <amaomi.prog@gmail.com>
2023-02-22 21:48:46 +0300

    Advance project structure

---
+++ src/backend/Browser.cpp
@@ -0,0 +1,25 @@
+/****************************************************************************
+ * Copyright (c) 2022 Andrey Sikorin, Ivan Grigorik                         *
+ *                                                                          *
+ * This program is free software: you can redistribute it and/or modify     *
+ * it under the terms of the GNU General Public License as published by     *
+ * the Free Software Foundation, version 3.                                 *
+ *                                                                          *
+ * This program is distributed in the hope that it will be useful, but      *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU         *
+ * General Public License for more details.                                 *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License        *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.     *
+ ****************************************************************************/
+
+#include "Browser.hpp"
+
+Browser::Browser(int argc, char* argv[])
+    : m_core { argc, argv }
+{
+    m_qmlEngine.load(u"qrc:/base/main.qml"_qs);
+}
+
+int Browser::run() { return QGuiApplication::exec(); }
---
+++ src/backend/include/Browser.hpp
@@ -0,0 +1,31 @@
+/****************************************************************************
+ * Copyright (c) 2022 Andrey Sikorin, Ivan Grigorik                         *
+ *                                                                          *
+ * This program is free software: you can redistribute it and/or modify     *
+ * it under the terms of the GNU General Public License as published by     *
+ * the Free Software Foundation, version 3.                                 *
+ *                                                                          *
+ * This program is distributed in the hope that it will be useful, but      *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU         *
+ * General Public License for more details.                                 *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License        *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.     *
+ ****************************************************************************/
+
+#pragma once
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+class Browser {
+public:
+    Browser(int argc, char* argv[]);
+
+    [[nodiscard]] static int run();
+
+private:
+    QGuiApplication m_core;
+    QQmlApplicationEngine m_qmlEngine;
+};
